0f565134a9a4849d5bf07d548f2f4f97dddaedc8
zackluckyf <fanning.34@gmail.com>
2017-07-14 01:24:23 -0400

    compression commented out

--- server/server.ts
+++ server/server.ts
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Gzip
-app.use(compression());
+// app.use(compression());s
 
 // Set our api routes
 app.use('/api', api);
