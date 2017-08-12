84f1bb6882243f1a51257d642bdc62f83ed85434
zackluckyf <fanning.34@gmail.com>
2017-08-11 21:13:10 -0400

    setup compression of the server

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -26,5 +27,14 @@ module.exports = {
     rules: [
       { test: /\.ts$/, loader: 'ts-loader' }
     ]
-  }
+  },
+  plugins: [
+		new CompressionPlugin({
+			asset: "[path].gz[query]",
+			algorithm: "gzip",
+			test: /\.(js|html)$/,
+			threshold: 10240,
+			minRatio: 0.8
+		})
+	]
 }
