d7fb4e10c47479d20e4d721bcab4194fa25d740f
zackluckyf <fanning.34@gmail.com>
2017-09-28 20:09:11 -0400

    setup posts on the dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { SharedModule } from '../shared/shared.module';
 
+import { DashboardService } from './shared/services/dashboard.service';
+
 import { DashboardRoutingModule} from './dashboard-routing.module';
 import { ShellComponent } from './shell/shell.component';
 
@@ -12,6 +14,7 @@ import { ShellComponent } from './shell/shell.component';
     DashboardRoutingModule,
     SharedModule
   ],
-  declarations: [ShellComponent]
+  declarations: [ShellComponent],
+  providers: [DashboardService]
 })
 export class DashboardModule { }
--- src/app/dashboard/shell/shell.component.ts
+++ src/app/dashboard/shell/shell.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/share';
+
+import { Posts } from '../shared/models/posts';
+import { DashboardService } from '../shared/services/dashboard.service';
+
 @Component({
   selector: 'dashboard-shell',
   templateUrl: './shell.component.html',
@@ -8,9 +14,12 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class ShellComponent implements OnInit {
 
-  constructor() { }
+  posts$: Observable<Posts[]>;
+
+  constructor(private _dashboardService: DashboardService) { }
 
   ngOnInit() {
+    this.posts$ = this._dashboardService.getAllUserPosts(1).share();
   }
 
 }
