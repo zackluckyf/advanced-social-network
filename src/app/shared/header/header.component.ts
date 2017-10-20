a67a019eedaf94304f5836d85d0b17e3b192a51e
zackluckyf <fanning.34@gmail.com>
2017-10-19 22:05:11 -0400

    logout added to header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { HeaderService } from './header.service';
+
 @Component({
   selector: 'shared-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  providers: [ HeaderService ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private _headerService: HeaderService) { }
 
   ngOnInit() {  }
 
   nav(location: string){
     this.router.navigate([location]);
+  }
+
+  logout(){
+    this._headerService.logout().subscribe(data => {
+      this.nav(data.location);
+    });
   }
 
 }
---
+++ src/app/shared/header/header.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class HeaderService {
+
+    constructor(private http: HttpClient) { }
+
+    logout(): Observable<any> {
+        const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
+        return this.http.get(`/api/logout`, { headers })
+                        .catch(error => Observable.throw(error));
+    }
+    
+}
