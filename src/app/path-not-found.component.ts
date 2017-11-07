f8cb6dde77f07f2697e91d1ff9c489bfcfac43b9
zackluckyf <fanning.34@gmail.com>
2017-11-07 17:33:44 -0500

    wip on path not found location

--- src/app/path-not-found.component.ts
+++ src/app/path-not-found.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PlatformLocation } from '@angular/common';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,26 +7,19 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './path-not-found.component.html',
   styleUrls: ['./path-not-found.component.scss']
 })
+
 export class PathNotFoundComponent implements OnInit {
 
   status: number = 404;
   statusText: string = 'The page you are looking for does not exist';
-  url: string = this.platformLocation.pathname;
+  url: string;
 
-  constructor(private router: Router, private route: ActivatedRoute, private platformLocation: PlatformLocation) { 
-
-  }
+  constructor(private router: Router) { }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-        console.log(params);
-        let paramStatus = params.get('status');
-        let paramStatusText = decodeURIComponent(params.get('statusText') === null ? '': params.get('statusText'));
-        let paramUrl = params.get('url');
-        this.status = Number(paramStatus) || this.status;
-        this.statusText = paramStatusText || this.statusText;
-        this.url = paramUrl || this.url;
-    })
+    this.router.events.subscribe((val) => {
+      this.url = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
+    });
   }
 
 }
