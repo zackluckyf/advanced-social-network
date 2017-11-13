d70dc725f1a739be68a12f9d30febe809a39fb41
zackluckyf <fanning.34@gmail.com>
2017-11-13 18:58:02 -0500

    removed try catch from db queries

--- server/queries/users/users.ts
+++ server/queries/users/users.ts
@@ -2,174 +2,130 @@ var models = require('../../../models').getModels();
 import * as moment from 'moment';
 
 let getUserNameAndBirthday = async (userId: number) => {
-    try {
-      let res = await models.users.find({
-        attributes: [ 'firstName', 'lastName', 'birthDate' ],
-        where: {
-          id: userId
-        }
-      });
-      return res;
-    } catch(e) {
-      e => console.log(e.stack)
-    } 
+    let res = await models.users.find({
+      attributes: [ 'firstName', 'lastName', 'birthDate' ],
+      where: {
+        id: userId
+      }
+    });
+    return res;
 };
 
 let getAllUsers = async () => {
-  try {
-    let res = await models.users.findAll({
-        attributes: [ 'firstName', 'lastName' ]
-      });
-      return res;
-    } catch(e) {
-      e => console.log(e.stack)
-    };
+  let res = await models.users.findAll({
+      attributes: [ 'firstName', 'lastName' ]
+    });
+    return res;
 };
 
 let getUserPosts = async (userId: number) => {
-  try {
-    let res = await models.posts.findAll({
-      attributes: [ 'postText' ],
-      where: {
-        user_id: userId
-      }
-    });
-    return res;
-  } catch(e) {
-    e => console.log(e.stack)
-  };
+  let res = await models.posts.findAll({
+    attributes: [ 'postText' ],
+    where: {
+      user_id: userId
+    }
+  });
+  return res;
 };
 
 let getUserComments = async (userId: number) => {
-  try {
-    let res = await models.comments.findAll({
-      attributes: [ 'commentText' ],
-      where: {
-        user_id: userId
-      }
-    });
-    return res;
-  } catch(e) {
-    e => console.log(e.stack)
-  };
+  let res = await models.comments.findAll({
+    attributes: [ 'commentText' ],
+    where: {
+      user_id: userId
+    }
+  });
+  return res;
 };
 
 let deleteUser = async (name: string) => {
-  try {
     let res = await models.test_data.destroy({
       where: {
         name: name
       }
     });
     return res;
-  } catch(e) {
-    e => console.log(e.stack)
-  };
 }
 
 let createUser = async (user: any) => {
-  try {
-    let res = await models.users.create({
-        firstName: user.firstName,
-        lastName: user.lastName,
-        birthday: user.birthday,
-        username: user.username,
-        email: user.email,
-        password: user.password
-    });
-    return res;
-  } catch(e) {
-    e => console.log(e.stack)
-  };
+  let res = await models.users.create({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      birthday: user.birthday,
+      username: user.username,
+      email: user.email,
+      password: user.password
+  });
+  return res;
 }
 
 let changeUserAge = async (user: any) => {
-  try {
-    let res = await models.test_data.update({
-      age: user.age
-    }, {
-      where: {
-        name: user.name
-      }, 
-      returning: true,
-      plain: true
-    });
-    return res;
-  } catch(e) {
-    e => console.log(e.stack)
-  };
+  let res = await models.test_data.update({
+    age: user.age
+  }, {
+    where: {
+      name: user.name
+    }, 
+    returning: true,
+    plain: true
+  });
+  return res;
 };
 
 let getListOfUsers = async (query: string) => {
-  try {
-    let res = await models.users.findAll({
-        attributes: [ 'id', 'firstName', 'lastName' ],
-        where: {
-          $or: [
-            {
-              first_name: {
-                $ilike: `%${query}%`
-              }
-            },
-            {
-              last_name: {
-                $ilike: `%${query}%`
-              }
-            }
-          ]
-        },
-        limit: 5
-      });
-    return res;
-  } catch(e) {
-    e => console.log(e.stack)
-  };
-};
-
-let getUser = async (email: string) => {
-  try {
-    let res = await models.users.findOne({ 
+  let res = await models.users.findAll({
+      attributes: [ 'id', 'firstName', 'lastName' ],
       where: {
         $or: [
           {
-            email: email
+            first_name: {
+              $ilike: `%${query}%`
+            }
           },
           {
-            username: email
+            last_name: {
+              $ilike: `%${query}%`
+            }
           }
         ]
-      }
+      },
+      limit: 5
     });
-    return res;
-  } catch(e) {
-    e => console.log(e.stack)
-  };
+  return res;
+};
+
+let getUser = async (email: string) => {
+  let res = await models.users.findOne({ 
+    where: {
+      $or: [
+        {
+          email: email
+        },
+        {
+          username: email
+        }
+      ]
+    }
+  });
+  return res;
 };
 
 let getUserJwt = async (jwt: string) => {
-  try {
-    let res = await models.users.findOne({ 
-      where: {
-        id: jwt
-      }
-    });
-    return res;
-  } catch(e) {
-    e => console.log(e.stack)
-  };
+  let res = await models.users.findOne({ 
+    where: {
+      id: jwt
+    }
+  });
+  return res;
 };
 
 let changePassword = async (passwordResetToken: string) => {
-  try {
-    let res = await models.users.findOne({
-      where: {
-        reset_password_token: passwordResetToken
-      }
-    });
-    return res;
-  } catch(e) {
-    e => console.log(e.stack)
-  };
+  let res = await models.users.findOne({
+    where: {
+      reset_password_token: passwordResetToken
+    }
+  });
+  return res;
 };
 
 module.exports = {
